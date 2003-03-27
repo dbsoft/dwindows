22886bf7e30aa2a588dc4fe22e220211c671630a
bsmith@81767d24-ef19-dc11-ae90-00e081727c95 <bsmith@81767d24-ef19-dc11-ae90-00e081727c95>
2003-03-27 11:14:25 +0000

    Filled in many of the holes on OS/2.

--- dw.h
+++ dw.h
@@ -142,39 +142,39 @@ typedef struct _user_data
 #define VK_RBUTTON           VK_BUTTON2
 #define VK_MBUTTON           VK_BUTTON3
 #define VK_RETURN            VK_NEWLINE
-#define VK_PRINT             VK_PRINTSCRN
-#define VK_CANCEL            128
-#define VK_CAPITAL           129
-#define VK_ESCAPE            130
-#define VK_PRIOR             131
-#define VK_NEXT              132
+#define VK_SNAPSHOT          VK_PRINTSCRN
+#define VK_CANCEL            VK_BREAK
+#define VK_CAPITAL           VK_CAPSLOCK
+#define VK_ESCAPE            VK_ESC
+#define VK_PRIOR             VK_PAGEUP
+#define VK_NEXT              VK_PAGEDOWN
 #define VK_SELECT            133
 #define VK_EXECUTE           134
-#define VK_SNAPSHOT          135
+#define VK_PRINT             135
 #define VK_HELP              136
 #define VK_LWIN              137
 #define VK_RWIN              138
-#define VK_MULTIPLY          139
-#define VK_ADD               140
+#define VK_MULTIPLY          ('*' + 128)
+#define VK_ADD               ('+' + 128)
 #define VK_SEPARATOR         141
-#define VK_SUBTRACT          142
-#define VK_DECIMAL           143
-#define VK_DIVIDE            144
-#define VK_SCROLL            145
-#define VK_LSHIFT            146
+#define VK_SUBTRACT          ('-' + 128)
+#define VK_DECIMAL           ('.' + 128)
+#define VK_DIVIDE            ('/' + 128)
+#define VK_SCROLL            VK_SCRLLOCK
+#define VK_LSHIFT            VK_SHIFT
 #define VK_RSHIFT            147
-#define VK_LCONTROL          148
+#define VK_LCONTROL          VK_CTRL
 #define VK_RCONTROL          149
-#define VK_NUMPAD0           150
-#define VK_NUMPAD1           151
-#define VK_NUMPAD2           152
-#define VK_NUMPAD3           153
-#define VK_NUMPAD4           154
-#define VK_NUMPAD5           155
-#define VK_NUMPAD6           156
-#define VK_NUMPAD7           157
-#define VK_NUMPAD8           158
-#define VK_NUMPAD9           159
+#define VK_NUMPAD0           ('0' + 128)
+#define VK_NUMPAD1           ('1' + 128)
+#define VK_NUMPAD2           ('2' + 128)
+#define VK_NUMPAD3           ('3' + 128)
+#define VK_NUMPAD4           ('4' + 128)
+#define VK_NUMPAD5           ('5' + 128)
+#define VK_NUMPAD6           ('6' + 128)
+#define VK_NUMPAD7           ('7' + 128)
+#define VK_NUMPAD8           ('8' + 128)
+#define VK_NUMPAD9           ('9' + 128)
 
 
 typedef struct _window_data {
