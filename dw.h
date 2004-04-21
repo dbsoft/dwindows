11c0966ddd6eaeaaa437a96e133f08e12cf39254
bsmith@81767d24-ef19-dc11-ae90-00e081727c95 <bsmith@81767d24-ef19-dc11-ae90-00e081727c95>
2004-04-21 04:56:34 +0000

    Moved back the version to 0.9.0 preparing for a beta before the final 1.0 release.

--- dw.h
+++ dw.h
@@ -4,8 +4,8 @@
 #define _H_DW
 
 /* Dynamic Windows version numbers */
-#define DW_MAJOR_VERSION 1
-#define DW_MINOR_VERSION 0
+#define DW_MAJOR_VERSION 0
+#define DW_MINOR_VERSION 9
 #define DW_SUB_VERSION 0
 
 /* These corespond to the entries in the color
