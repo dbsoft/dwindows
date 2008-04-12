a5b12e5ab960691fecb8c37c604b513d4f2f2e37
mhessling <mhessling@81767d24-ef19-dc11-ae90-00e081727c95>
2008-04-12 06:45:55 +0000

    Update package version
    git-svn-id: https://svn.netlabs.org/repos/dwindows/trunk@626 81767d24-ef19-dc11-ae90-00e081727c95

--- dw.h
+++ dw.h
@@ -4,9 +4,9 @@
 #define _H_DW
 
 /* Dynamic Windows version numbers */
-#define DW_MAJOR_VERSION 0
-#define DW_MINOR_VERSION 9
-#define DW_SUB_VERSION 2
+#define DW_MAJOR_VERSION 1
+#define DW_MINOR_VERSION 1
+#define DW_SUB_VERSION 0
 
 /* These corespond to the entries in the color
  * arrays in the Win32 dw.c, they are also the
