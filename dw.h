12d1e9d30d222380db77fd4d94502cf98764097b
bsmith <bsmith@81767d24-ef19-dc11-ae90-00e081727c95>
2011-03-01 04:37:11 +0000

    MLE was missing the scrollbar. Fixes for getting and setting the position. However it still currently does not work until the window is shown.
    git-svn-id: https://svn.netlabs.org/repos/dwindows/trunk@679 81767d24-ef19-dc11-ae90-00e081727c95

--- dw.h
+++ dw.h
@@ -304,7 +304,7 @@ typedef struct _hpixmap {
    HWND handle;
 } *HPIXMAP;
 
-#define DW_DT_LEFT               0
+#define DW_DT_LEFT               0 /* NSLeftTextAlignment */
 #define DW_DT_QUERYEXTENT        0
 #define DW_DT_UNDERSCORE         0
 #define DW_DT_STRIKEOUT          0
@@ -312,7 +312,7 @@ typedef struct _hpixmap {
 #define DW_DT_EXTERNALLEADING    0
 #define DW_DT_CENTER             2 /* NSCenterTextAlignment */
 #define DW_DT_RIGHT              1 /* NSCenterTextAlignment */
-#define DW_DT_TOP                0 /* NSLeftTextAlignment */
+#define DW_DT_TOP                0 
 #define DW_DT_VCENTER            0
 #define DW_DT_BOTTOM             0
 #define DW_DT_HALFTONE           0
