179012b80ede7cc60cb5800fdc4951850097dd12
bsmith@81767d24-ef19-dc11-ae90-00e081727c95 <bsmith@81767d24-ef19-dc11-ae90-00e081727c95>
2011-09-16 06:58:18 +0000

    Update configure script and fix a miscommit of the GTK3 code.

--- gtk3/dw.c
+++ gtk3/dw.c
@@ -9584,7 +9584,7 @@ void dw_environment_query(DWEnv *env)
    env->DWMajorVersion = DW_MAJOR_VERSION;
    env->DWMinorVersion = DW_MINOR_VERSION;
 #ifdef VER_REV
-   env->DWSubVersion = atoi(VER_REV);
+   env->DWSubVersion = VER_REV;
 #else
    env->DWSubVersion = DW_SUB_VERSION;
 #endif
