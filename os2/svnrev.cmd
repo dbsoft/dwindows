80352dddf63ae219ec73e21a387cf15ba1d01326
mhessling@81767d24-ef19-dc11-ae90-00e081727c95 <mhessling@81767d24-ef19-dc11-ae90-00e081727c95>
2011-09-18 09:48:57 +0000

    Change to method of obtaining SVN revision number

--- os2/svnrev.cmd
+++ os2/svnrev.cmd
@@ -1,16 +1,16 @@
-/* REXX script to get the svn revision and save it to SVN.REV. */
-"svnversion -n . | rxqueue"
-/* Using PARSE PULL preserves case */
-Parse Pull sval
-/* If it is a double value get the first value */
-Parse Var sval ver ':' newver 'M' 'S'
-/* Remove trailing M or S from the version */
-Parse Var ver tmpver 'M'
-Parse Var tmpver ver 'S'
-err = LINEOUT('SVN.REV', 'VERREV=' || ver, 1)
-/* Check for an error. */
-IF err \== 0 THEN DO
-Bad:
-   SAY "ERROR:" STREAM('SVN.REV', 'D')
-   RETURN
-END
+/* REXX script to get the svn revision and display it. */
+Trace o
+Address System 'svnversion . | rxqueue > nul:'
+/* default version to 0, if svnversion doesn't exist or no .svn here */
+ver = 0
+If rc = 0 Then
+   Do
+      /* Using PARSE PULL preserves case */
+      /* If it is a double value get the first value only */
+      Parse Pull sval ver ':' .
+      If Strip( ver ) = '' Then ver = sval
+      If ver = 'exported' Then ver = 0
+      If Datatype( Right( ver, 1 ) ) \= 'NUM' Then ver = Substr( ver, 1, Length( ver) - 1 )
+   End
+Say ver
+Exit 0
