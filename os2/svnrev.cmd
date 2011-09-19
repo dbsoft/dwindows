340ec62f4abf4d7a6aebd3270f99f9a8940bcb08
mhessling@81767d24-ef19-dc11-ae90-00e081727c95 <mhessling@81767d24-ef19-dc11-ae90-00e081727c95>
2011-09-19 09:52:45 +0000

    Revert to using a file as the source for SVN revision number.

--- os2/svnrev.cmd
+++ os2/svnrev.cmd
@@ -1,9 +1,10 @@
 /* REXX script to get the svn revision and display it. */
 Trace o
+fn = 'SVN.REV'
 Address System 'svnversion . | rxqueue > nul:'
 /* default version to 0, if svnversion doesn't exist or no .svn here */
 ver = 0
-If rc = 0 Then
+If Queued() \= 0 Then
    Do
       /* Using PARSE PULL preserves case */
       /* If it is a double value get the first value only */
@@ -12,5 +13,7 @@ If rc = 0 Then
       If ver = 'exported' Then ver = 0
       If Datatype( Right( ver, 1 ) ) \= 'NUM' Then ver = Substr( ver, 1, Length( ver) - 1 )
    End
-Say ver
+Call Stream fn, 'C', 'OPEN'
+Call Lineout fn,'VERREV='ver
+Call Stream fn, 'C', 'CLOSE'
 Exit 0
