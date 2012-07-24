c56da145088e3bc27127be26d63f0e77f7f7033e
bsmith <bsmith@81767d24-ef19-dc11-ae90-00e081727c95>
2012-07-24 22:52:57 +0000

    Put test program code into DEPRECATED #ifdef again to avoid build warnings. Added UTF-8/Wide string conversion functions which will be used in an editor.
    git-svn-id: https://svn.netlabs.org/repos/dwindows/trunk@1790 81767d24-ef19-dc11-ae90-00e081727c95

--- dwtest.c
+++ dwtest.c
@@ -1380,6 +1380,7 @@ void create_button( int redraw)
     }
 }
 
+#ifdef DEPRECATED
 void mdi_add(void)
 {
     HWND mdibox, mdi, mdi1w, mdi1box, ef, mdi2w, mdi2box, bb;
@@ -1411,6 +1412,7 @@ void mdi_add(void)
     dw_window_set_size(mdi2w, 200, 200);
     dw_window_show(mdi2w);
 }
+#endif
 
 void menu_add(void)
 {
@@ -1666,7 +1668,9 @@ int main(int argc, char *argv[])
     ULONG notebookpage3;
     ULONG notebookpage4;
     ULONG notebookpage5;
+#ifdef DEPRECATED
     ULONG notebookpage6;
+#endif
     ULONG notebookpage7;
     ULONG notebookpage8;
     ULONG notebookpage9;
@@ -1720,11 +1724,13 @@ int main(int argc, char *argv[])
     dw_notebook_page_set_text( notebook, notebookpage5, "buttons");
     buttons_add();
 
+#ifdef DEPRECATED
     notebookbox6 = dw_box_new( DW_VERT, 5 );
     notebookpage6 = dw_notebook_page_new( notebook, 1, FALSE );
     dw_notebook_pack( notebook, notebookpage6, notebookbox6 );
     dw_notebook_page_set_text( notebook, notebookpage6, "mdi");
     mdi_add();
+#endif
 
     notebookbox7 = dw_box_new( DW_VERT, 6 );
     notebookpage7 = dw_notebook_page_new( notebook, 1, FALSE );
