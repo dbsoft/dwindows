8b74f97034fea0d594ba326f500e9984eeef69b7
bsmith@81767d24-ef19-dc11-ae90-00e081727c95 <bsmith@81767d24-ef19-dc11-ae90-00e081727c95>
2020-06-17 03:38:21 +0000

    Win: Fixed the notification callback when clicking the toast. Currently, the toast isn't clickable once it moves into the action center. This is a known issue with WinToast, because once the toast moves into the action center, it requires a COM notificaiton object be registered.

--- win/wintoast.cpp
+++ win/wintoast.cpp
@@ -7,9 +7,6 @@ using namespace WinToastLib;
 extern "C" {
    LRESULT CALLBACK _wndproc(HWND hWnd, UINT msg, WPARAM mp1, LPARAM mp2);
    void dw_signal_disconnect_by_window(HWND window);
-#ifdef DEBUG
-   void dw_debug(const char *format, ...);
-#endif
 }
 
 class DWHandler : public IWinToastHandler {
@@ -18,40 +15,30 @@ public:
 
     void toastActivated() const {
         // The user clicked in this toast
-#ifdef DEBUG
-        dw_debug("Sending notification to DW eventhandler\n");
-#endif
         _wndproc((HWND)templ, WM_USER+102, 0, 0);
+        dw_signal_disconnect_by_window((HWND)templ);
+        delete templ;
     }
 
     void toastActivated(int actionIndex) const {
         // The user clicked on action
-#ifdef DEBUG
-        dw_debug("Sending notification to DW eventhandler via action\n");
-#endif
         _wndproc((HWND)templ, WM_USER+102, 0, 0);
+        dw_signal_disconnect_by_window((HWND)templ);
+        delete templ;
     }
 
     void toastDismissed(WinToastDismissalReason state) const {
         switch (state) {
         case UserCanceled:
             // The user dismissed this toast
-#ifdef DEBUG
-            dw_debug("The user dismissed this toast\n");
-#endif
+            dw_signal_disconnect_by_window((HWND)templ);
             delete templ;
             break;
         case TimedOut:
             // The toast has timed out
-#ifdef DEBUG
-            dw_debug("The toast has timed out\n");
-#endif
             break;
         case ApplicationHidden:
             // The application hid the toast using ToastNotifier.hide()
-#ifdef DEBUG
-            dw_debug("The application hid the toast using ToastNotifier.hide()\n");
-#endif
             break;
         default:
             // Toast not activated
@@ -61,9 +48,6 @@ public:
 
     void toastFailed() const {
         // Error showing current toast
-#ifdef DEBUG
-        dw_debug("Toast failed freeing template\n");
-#endif
         delete templ;
     }
 };
@@ -89,8 +73,19 @@ extern "C" {
    {
       if(WinToast::isCompatible()) 
       {
+         // Generate a Microsoft compatible Application User Model ID
+         LPWSTR company = wcschr(AppID, '.');
+         *company = 0;
+         LPWSTR product = wcschr(++company, '.');
+         *product = 0;
+         LPWSTR subproduct = wcschr(++product, '.');
+         if(subproduct)
+                *subproduct = 0;
+         LPWSTR version = subproduct ? wcschr(++subproduct, '.') : NULL;
+
          WinToast::instance()->setAppName(AppName);
-         WinToast::instance()->setAppUserModelId(AppID);
+         WinToast::instance()->setAppUserModelId(WinToast::instance()->configureAUMI(company, product, 
+             subproduct ? subproduct : L"", (version && version++ && _wtoi(version) > 0) ? version : L""));
          WinToast::instance()->initialize();
       }
    }
