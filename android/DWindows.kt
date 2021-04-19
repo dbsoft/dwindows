c63e5da5a7b3812ab868c6d375fe81c68ab71912
bsmith <bsmith@81767d24-ef19-dc11-ae90-00e081727c95>
2021-04-19 23:06:25 +0000

    Android: Implement thread, semaphore, shared memory API functions. Implement dwmain() entrypoint and include dwtest.c in the build. Like on iOS dw_main() doesn't actually run the message loop... So wait for dw_main() to be called and return to the JVM.
    git-svn-id: https://svn.netlabs.org/repos/dwindows/trunk@2485 81767d24-ef19-dc11-ae90-00e081727c95

--- android/DWindows.kt
+++ android/DWindows.kt
@@ -1,27 +1,37 @@
 package org.dbsoft.dwindows.dwtest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class DWindows : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+class DWindows : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dwindows_main)
 
+        val m = packageManager
+        var s = packageName
+        val p = m.getPackageInfo(s!!, 0)
+        s = p.applicationInfo.dataDir
+
         // Example of a call to a native method
-        findViewById<TextView>(R.id.sample_text).text = stringFromJNI()
+        findViewById<TextView>(R.id.sample_text).text = dwindowsInit(s)
     }
 
     /**
-     * A native method that is implemented by the 'dwindows' native library,
+     * Native methods that are implemented by the 'dwindows' native library,
      * which is packaged with this application.
      */
-    external fun stringFromJNI(): String
+    external fun dwindowsInit(dataDir: String): String
 
-    companion object {
+    companion object
+    {
         // Used to load the 'dwindows' library on application startup.
-        init {
+        init
+        {
             System.loadLibrary("dwindows")
         }
     }
