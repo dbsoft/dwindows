ce67feb991704041f1a644970fd8c01809179641
bsmith@81767d24-ef19-dc11-ae90-00e081727c95 <bsmith@81767d24-ef19-dc11-ae90-00e081727c95>
2022-12-18 12:13:46 +0000

    C++: Add text widget and packing to the window. Also reformat both files using spaces instead of tabs.

--- dwtestoo.cpp
+++ dwtestoo.cpp
@@ -1,25 +1,31 @@
+/*
+ * Simple C++ Dynamic Windows Example
+ */
 #include <dw.hpp>
 
 class MyWindow : public DW::Window
 {
 public:
-  MyWindow() {
-	SetText("Basic application");
-	SetSize(200, 200);
-	}
-	virtual int OnDelete() override { DW::App *app = DW::App::Init(); app->MainQuit(); return FALSE; }
-	virtual int OnConfigure(int width, int height) override { return FALSE; }
+    MyWindow() {
+        SetText("Basic application");
+        SetSize(200, 200);
+     }
+     virtual int OnDelete() override { DW::App *app = DW::App::Init(); app->MainQuit(); return FALSE; }
+     virtual int OnConfigure(int width, int height) override { return FALSE; }
 };
 
-int dwmain(int argc, char* argv[])
+int dwmain(int argc, char* argv[]) 
 {
-  DW::App *app = DW::App::Init(argc, argv, "org.dbsoft.dwindows.dwtestoo");
-  MyWindow *window = new MyWindow();
+    DW::App *app = DW::App::Init(argc, argv, "org.dbsoft.dwindows.dwtestoo");
+    MyWindow *window = new MyWindow();
+    DW::Text *text = new DW::Text("Test window");
+    
+    window->PackStart(text, DW_SIZE_AUTO, DW_SIZE_AUTO, TRUE, TRUE, 0);
+    text->SetStyle(DW_DT_CENTER | DW_DT_VCENTER, DW_DT_CENTER | DW_DT_VCENTER);
+    window->Show();
 
-  window->Show();
+    app->Main();
+    app->Exit(0);
 
-  app->Main();
-  app->Exit(0);
-
-  return 0;
+    return 0;
 }
